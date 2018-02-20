ca2d4f841252a777bd16d6f7d35d13fdaa7f6e04
Tom Rico <trico@Toms-MacBook-Pro.local>
2018-02-20 09:56:29 -0800

    update

---
+++ src/ExecuteShell.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ExecuteShell {
+
+	/*public static void main(String[] args) {
+
+		ExecuteShell obj = new ExecuteShell();
+
+		//String domainName = "google.com";
+		//in mac oxs
+		//String command = "ping -c 10 " + domainName;
+		
+		String command = "rpm -Uvh \\ https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm";
+
+		//in windows
+		//String command = "ping -n 3 " + domainName;
+
+		String output = obj.executeCommand(command);
+
+		System.out.print(output);
+
+	}*/
+
+	@SuppressWarnings("unused")
+	public String executeCommand(String command) {
+
+		StringBuffer output = new StringBuffer();
+
+		Process p;
+		try {
+			p = Runtime.getRuntime().exec(command);
+			p.waitFor();
+			BufferedReader reader =
+                            new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+                        String line = "";
+			while ((line = reader.readLine())!= null) {
+				output.append(line + "\n");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return output.toString();
+
+	}
+
+}
---
+++ src/RhoInstall.java
@@ -0,0 +1,122 @@
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class RhoInstall extends ExecuteShell{
+	
+	
+	public static void main( String[] args )
+	{
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner( System.in );
+		
+		//RhoSetup myRhoSetup = new RhoSetup();
+		
+		System.out.print("\nWelcome to the RHO Setup Quick Start installer Version 1.0\n\n");
+		
+		System.out.print("Rho is a tool that identifies information about your Red Hat®\n" +
+					    "Enterprise Linux® (RHEL), Red Hat JBoss® Enterprise Application\n" +
+					    "Platform, Red Hat JBoss Fuse, Red Hat JBoss AMQ, and Red Hat JBoss\n" +
+					    "BRMS product deployment environment. This information includes\n" +
+					    "characteristics related to hardware, configuration, physical and\n" + 
+				        "virtual systems, operating systems, Red Hat packages, and other data.\n" +
+					    "The information that is contained in this Quick Start Guide explains\n" + 
+					    "how to install Rho, set up authentication and network profiles, scan\n" + 
+				        	"your environment, and view the results of the scan.\n\n");
+		
+		
+	    //Determine which Linux distribution on target system for installation of RHO
+		
+		System.out.print("Step 1\n");
+		System.out.print("\n***Log-in as Root***\n\n");
+		System.out.print("Install EPEL-Release Packages (Extra Packages for Enterprise Linux)\n\n");
+		
+		System.out.print("Rho is supported by the following Linux distributions\n");
+		System.out.print(" 1)Red Hat Enterprise Linux 6\n");
+		System.out.print(" 2)Red Hat Enterprise Linux 7\n");
+		System.out.print(" 3)CentOS 6\n");
+		System.out.print(" 4)CentOS 7\n");
+		
+		System.out.print("\nEnter the number associated with your linux distribution: ");
+		String distro = input.nextLine();
+		int distroInt = Integer.parseInt(distro);
+	
+		
+		String yes_no;
+		ExecuteShell obj = new ExecuteShell();
+		
+			switch(distroInt)
+			{
+				case 1:
+					System.out.print("\nYou've selected RHEL 6, do you wish do download and install these packages?(type \"y\" or \"yes\")");
+					yes_no = input.nextLine();
+					if(yes_no.contains("y")|| yes_no.contains("yes")) {
+						System.out.print("Installing extra packages\n");
+						//Execute Bash Command
+						String loadEpel = "rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm";
+						String output = obj.executeCommand(loadEpel);
+						
+						System.out.printf("%s", output);
+					} 
+					if(yes_no.contains("n") || yes_no.contains("no")){
+						System.out.print("Installation Cancelled\n");
+					}
+					break;
+				case 2:
+					System.out.print("\nYou've selected RHEL 7, do you wish do download these packages?(type \"y\" or \"yes\")");
+					yes_no = input.nextLine();
+					if(yes_no.contains("y")|| yes_no.contains("yes")) {
+						System.out.print("Installing extra packages\n");
+						//Execute Bash Command
+						String loadEpel = "rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm";
+						String output = obj.executeCommand(loadEpel);
+						System.out.print(output);
+					} 
+					if(yes_no.contains("n") || yes_no.contains("no")){
+						System.out.print("Installation Cancelled\n");
+					}
+					break;
+				case 3:
+					System.out.print("\nYou've selected CentOS 6, do you wish do download these packages?(type \"y\" or \"yes\")");
+					yes_no = input.nextLine();
+					if(yes_no.contains("y")|| yes_no.contains("yes")) {
+						System.out.print("Installing extra packages\n");
+						//Execute Bash Command
+						String loadEpel = "rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm";
+						String output = obj.executeCommand(loadEpel);
+						System.out.print(output);
+					} 
+					if(yes_no.contains("n") || yes_no.contains("no")){
+						System.out.print("Installation Cancelled\n");
+					}
+					break;
+				case 4:
+					System.out.print("\nYou've selected CentOS 7, do you wish do download these packages?(type \"y\" or \"yes\")");
+					yes_no = input.nextLine();
+					if(yes_no.contains("y")|| yes_no.contains("yes")) {
+						System.out.print("Installing extra packages\n");
+						//Execute Bash Command
+						String loadEpel = "rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm";
+						String output = obj.executeCommand(loadEpel);
+						System.out.print(output);
+					} 
+					if(yes_no.contains("n") || yes_no.contains("no")){
+						System.out.print("Installation Cancelled\n");
+					}
+					break;
+			}
+		//Show EPEL has been downloaded prior to install
+		
+		try {
+			
+			String showEpel = "yum repolist epel*.rpm";
+			String output = obj.executeCommand(showEpel);
+			output.wait(4000);
+			System.out.printf("%s",output);
+				
+			}  catch (Exception e) {
+					
+			}
+		
+	}
+}
